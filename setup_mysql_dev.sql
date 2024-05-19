b63e02933f4d244175b80f9624581a4acc8c7ed7
Lamba237 <simontanyi2004@gmail.com>
2024-05-19 13:11:25 -0700

    updates

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
-#!/bin/bash
+# script that prepares a MySQL server for the project
 mysql -u root -p
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
