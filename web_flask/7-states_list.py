466a281305654ded87b13ff7343aafe42ab28533
Lamba237 <simontanyi2004@gmail.com>
2024-05-19 14:16:55 -0700

    updates

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-""" returns list of states """
-from flask import Flask
-from flask import render_template
+""" Starts a Flask web app """
+from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
-
-
-@app.route('/states_list', strict_slashes=False)
-def states_list():
-    """ returns list of states """
-    states = storage.all('State')
-    return render_template('7-states_list.html', states=states)
+app.url_map.strict_slashes = False
 
 
 @app.teardown_appcontext
-def teardown_db(exception):
-    """ closes the storage """
+def dispose(exception):
+    """ Remove current session """
     storage.close()
+
+
+@app.route('/states_list')
+def states():
+    """ Display list of all the states """
+    states = storage.all(State)
+    states_list = list(states.values())
+    return render_template('7-states_list.html', states=states_list)
 
 
 if __name__ == '__main__':
